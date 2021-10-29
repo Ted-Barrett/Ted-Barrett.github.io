aa2c35bc5350b9ac5ab7aec5402a40a9c3ec02b0
Ted Barrett <tlbarrett@student.unimelb.edu.au>
2021-10-29 22:47:51 +1100

    thank god for stackoverflow

--- ACCSetup.js
+++ ACCSetup.js
@@ -4,7 +4,28 @@ console.log(d.toString())
 document.getElementById('download').addEventListener('click', () => {
     let dl = document.createElement('a')
     dl.download = 'test.json' // target filename
-    
-    console.log(dl.href);
-    // dl.click()
+    dl.href = `data:application/json;charset=utf-8,${JSON.stringify(d,replacer)}`
+    dl.click()
 })
+
+
+// https://stackoverflow.com/a/56150320
+function replacer(key, value) {
+    if(value instanceof Map) {
+        return {
+            dataType: 'Map',
+            value: Array.from(value.entries()), // or with spread: value: [...value]
+        };
+    } else {
+        return value;
+    }
+}
+
+function reviver(key, value) {
+    if(typeof value === 'object' && value !== null) {
+        if (value.dataType === 'Map') {
+            return new Map(value.value);
+        }
+    }
+    return value;
+}
